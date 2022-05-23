414284fba77a3a10e99808a2194f3f7100bfde70
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-05-23 11:29:11 +0200

    23052022

--- ton_abspielen.ino
+++ ton_abspielen.ino
@@ -1,11 +1,16 @@
 int pip = 5;
+int led_red = 4;
 
 void setup() {
   pinMode(pip, OUTPUT);
+  pinMode(led_red, OUTPUT);
 }
 
 void loop() {
   digitalWrite(pip, HIGH);
-  delay(100);
+  digitalWrite(led_red, HIGH);
+  delay(500);
   digitalWrite(pip, LOW);
+  digitalWrite(led_red, LOW);
+  delay(500);
 }
